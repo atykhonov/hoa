653721c62fed04ca8b883c14634f97824d944b72
Andriy Tykhonov <atykhonov@gmail.com>
2017-02-25 03:24:49 +0200

    Restore wsgi.py.

--- osbb/wsgi.py
+++ osbb/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+root = os.path.join(os.path.dirname(__file__), '..')
+sys.path.insert(0, root)
+packages = '/var/www/.virtualenvs/hoa/lib/python3.4/site-packages'
+sys.path.insert(0, packages)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "osbb.settings")
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
